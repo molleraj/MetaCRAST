3f2d5864d8687e0b8ff0eaac18cf6e3251e57fe9
molleraj <mollera2@miamioh.edu>
2016-05-27 21:27:12 -0400

    Add files via upload

---
+++ getDRquery.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+my $usage = "getDRquery.pl DR_database_path query output_file";
+
+my $DR_database_path = $ARGV[0] or die $usage;
+my $query = $ARGV[1] or die $usage;
+my $output_file = $ARGV[2] or die $usage;
+
+print `grep --no-group-separator -E -A1 '$query' $DR_database_path > $output_file`; 
+#print $query."\n";
+#print $output_file."\n"; 
