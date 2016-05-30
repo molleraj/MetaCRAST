3128772d47d6c720daf0cb9578d37e56216f0d71
molleraj <mollera2@miamioh.edu>
2016-05-30 11:13:55 -0400

    Updated install script

--- local_install.sh
+++ local_install.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+# install cd-hit
+sudo apt-get install cd-hit
+
+# install CPAN packages 
+sudo perl -MCPAN -e install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop
+
+# add MetaCRAST and MetaCRAST/bin to PATH
