7199faf6fc1b032bc686f4f2b5df2a46eb4e40d9
molleraj <mollera2@miamioh.edu>
2016-06-02 16:20:54 -0400

    Update local_install.sh

--- local_install.sh
+++ local_install.sh
@@ -9,11 +9,9 @@ sudo apt-get install cd-hit
 sudo perl -MCPAN -e install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop
 
 # make bin files executable
-chmod 777 ~/MetaCRAST/bin/*
+sudo chmod 777 ~/MetaCRAST/bin/*
 
 # add MetaCRAST and MetaCRAST/bin to PATH
-export PATH="$HOME/MetaCRAST/bin:$PATH"
-echo "export PATH='$HOME/MetaCRAST/bin:$PATH'" >> ~/.bash_profile
+export PATH=$HOME/MetaCRAST/bin:$PATH
 echo "export PATH='$HOME/MetaCRAST/bin:$PATH'" >> ~/.profile
-source ~/.bash_profile
 source ~/.profile
