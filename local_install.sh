1f2af0ea9c70b68b2d6572ff65d971646760d47f
molleraj <mollera2@miamioh.edu>
2016-06-02 16:08:09 -0400

    Update local_install.sh

--- local_install.sh
+++ local_install.sh
@@ -1,3 +1,7 @@
+# How to install
+# IN HOME FOLDER: git clone https://github.com/molleraj/MetaCRAST.git
+# chmod 777 ~/MetaCRAST/local_install.sh
+
 #!/bin/sh
 
 # install cd-hit
@@ -7,11 +11,11 @@ sudo apt-get install cd-hit
 sudo perl -MCPAN -e install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop
 
 # make bin files executable
-chmod 777 ./bin/*
+chmod 777 ~/MetaCRAST/bin/*
 
 # add MetaCRAST and MetaCRAST/bin to PATH
 export PATH="$HOME/MetaCRAST:/MetaCRAST/bin:$PATH"
-echo "export PATH='$HOME/MetaCRAST:/MetaCRAST/bin:$PATH'" >> ~/.bash_profile
-echo "export PATH='$HOME/MetaCRAST:/MetaCRAST/bin:$PATH'" >> ~/.profile
+echo "export PATH='$HOME/MetaCRAST:$HOME/MetaCRAST/bin:$PATH'" >> ~/.bash_profile
+echo "export PATH='$HOME/MetaCRAST:$HOME/MetaCRAST/bin:$PATH'" >> ~/.profile
 source ~/.bash_profile
 source ~/.profile
