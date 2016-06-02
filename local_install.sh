985c58fe32268460352ad43cd6117cd360fb4324
molleraj <mollera2@miamioh.edu>
2016-06-02 15:26:33 -0400

    Update local_install.sh

--- local_install.sh
+++ local_install.sh
@@ -6,4 +6,7 @@ sudo apt-get install cd-hit
 # install CPAN packages 
 sudo perl -MCPAN -e install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop
 
+# make bin files executable
+chmod 777 ./bin/*
+
 # add MetaCRAST and MetaCRAST/bin to PATH
