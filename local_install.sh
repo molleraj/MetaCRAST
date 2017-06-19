ad6ed10d1c67c207275877682ca54105da26e6c8
Jon Moller <mollera2@miamioh.edu>
2017-06-18 21:22:14 -0400

    adding MetaCRASTs and MetaCRASTbp versions

--- local_install.sh
+++ local_install.sh
@@ -5,8 +5,8 @@
 # install cd-hit
 sudo apt-get install cd-hit
 
-# install CPAN packages 
-sudo cpan install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop 
+# install CPAN packages
+sudo cpan install Text::Levenshtein::XS String::Approx Getopt::Std Bio::SeqIO Bio::Perl MCE MCE::Loop MCE::Shared
 
 # install dependencies for fasta-splitter.pl
 sudo cpan install File::Util File::Path File::Basename Getopt::Long
